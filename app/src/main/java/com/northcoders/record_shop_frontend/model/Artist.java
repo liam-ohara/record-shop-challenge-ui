c54bd0814580d9f8c4ddd86f22d343e4e220c308
Liam O'Hara <liam.ohara@gmail.com>
2024-09-18 11:05:24 +0100

    Feat: implement Artist model class.

---
+++ app/src/main/java/com/northcoders/record_shop_frontend/model/Artist.java
@@ -0,0 +1,30 @@
+package com.northcoders.record_shop_frontend.model;
+
+public class Artist {
+
+    private Long artistId;
+    private String name;
+
+    public Artist() {}
+
+    public Artist(Long artistId, String name) {
+        this.artistId = artistId;
+        this.name = name;
+    }
+
+    public Long getArtistId() {
+        return artistId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setArtistId(Long artistId) {
+        this.artistId = artistId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
