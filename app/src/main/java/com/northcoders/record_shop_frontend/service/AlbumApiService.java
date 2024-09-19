3c995e357579d11b75e8aeb79c466f0a63e4cb60
Liam O'Hara <liam.ohara@gmail.com>
2024-09-19 11:01:18 +0100

    Feat: implement postAlbum method in AlbumApiService.

--- app/src/main/java/com/northcoders/record_shop_frontend/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/record_shop_frontend/service/AlbumApiService.java
@@ -5,10 +5,15 @@ import com.northcoders.record_shop_frontend.model.Album;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumApiService {
 
     @GET("album")
     Call<List<Album>> getAllAlbum ();
+
+    @POST("album")
+    Call<Album> postAlbum(@Body Album album);
 }
