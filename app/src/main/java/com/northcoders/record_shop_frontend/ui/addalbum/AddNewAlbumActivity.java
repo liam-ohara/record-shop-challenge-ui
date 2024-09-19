978c859c5ef0b4a93e45475e40bd062f33eaaacd
Liam O'Hara <liam.ohara@gmail.com>
2024-09-19 15:38:58 +0100

    Feat: implement onCreate method body in AddNewAlbumActivity.

--- app/src/main/java/com/northcoders/record_shop_frontend/ui/addalbum/AddNewAlbumActivity.java
+++ app/src/main/java/com/northcoders/record_shop_frontend/ui/addalbum/AddNewAlbumActivity.java
@@ -7,20 +7,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.northcoders.record_shop_frontend.R;
+import com.northcoders.record_shop_frontend.databinding.ActivityAddNewAlbumBinding;
+import com.northcoders.record_shop_frontend.model.Album;
+import com.northcoders.record_shop_frontend.ui.mainactivity.MainActivityViewModel;
 
 public class AddNewAlbumActivity extends AppCompatActivity {
+
+    private ActivityAddNewAlbumBinding newAlbumBinding;
+    private AddAlbumClickHandlers addAlbumClickHandlers;
+    private Album album;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_new_album);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        album = new Album();
+
+        newAlbumBinding = DataBindingUtil.setContentView(this, R.layout.activity_add_new_album);
+
+        MainActivityViewModel viewModel = new ViewModelProvider(this)
+                .get(MainActivityViewModel.class);
+
+        newAlbumBinding.setAlbum(album);
+
+        newAlbumBinding.setClickhandler(addAlbumClickHandlers);
+
     }
 }
