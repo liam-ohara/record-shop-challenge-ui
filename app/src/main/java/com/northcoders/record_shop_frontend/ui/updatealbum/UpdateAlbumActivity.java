7ac3255c4a9dd3674b402b6bf1b1e15122781b31
Liam O'Hara <liam.ohara@gmail.com>
2024-09-20 14:14:04 +0100

    Feat: implement UpdateAlbumActivity.

---
+++ app/src/main/java/com/northcoders/record_shop_frontend/ui/updatealbum/UpdateAlbumActivity.java
@@ -0,0 +1,21 @@
+package com.northcoders.record_shop_frontend.ui.updatealbum;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.northcoders.record_shop_frontend.R;
+
+public class UpdateAlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_album);
+
+    }
+}
