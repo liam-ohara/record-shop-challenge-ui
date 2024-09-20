773bebadb2661c60bb95deed0609ad16b263188b
Liam O'Hara <liam.ohara@gmail.com>
2024-09-20 12:32:04 +0100

    Feat: implement PATCH and DELETE endpoints in AlbumApiService.

--- app/src/main/java/com/northcoders/record_shop_frontend/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/record_shop_frontend/service/AlbumApiService.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface AlbumApiService {
 
@@ -16,4 +19,10 @@ public interface AlbumApiService {
 
     @POST("album")
     Call<Album> postAlbum(@Body Album album);
+
+    @PATCH("album/{id}")
+    Call<Album> updateAlbum(@Path("id") long id, @Body Album album);
+
+    @DELETE("album/{id}")
+    Call<Album> deleteAlbum(@Path("id") long id);
 }
