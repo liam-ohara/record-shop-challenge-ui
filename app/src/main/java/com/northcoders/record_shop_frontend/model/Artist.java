95578bb1280eb3a5745567e5888ec95b16b5490c
Liam O'Hara <liam.ohara@gmail.com>
2024-09-23 15:03:11 +0100

    Feat: implement Parcelable in Artist model class.

--- app/src/main/java/com/northcoders/record_shop_frontend/model/Artist.java
+++ app/src/main/java/com/northcoders/record_shop_frontend/model/Artist.java
@@ -1,12 +1,16 @@
 package com.northcoders.record_shop_frontend.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
 
 import com.google.gson.annotations.SerializedName;
 import com.northcoders.record_shop_frontend.BR;
 
-public class Artist extends BaseObservable {
+public class Artist extends BaseObservable implements Parcelable {
 
     @SerializedName("artistId")
     private Long artistId;
@@ -19,6 +23,27 @@ public class Artist extends BaseObservable {
         this.artistId = artistId;
         this.name = name;
     }
+
+    protected Artist(Parcel in) {
+        if (in.readByte() == 0) {
+            artistId = null;
+        } else {
+            artistId = in.readLong();
+        }
+        name = in.readString();
+    }
+
+    public static final Creator<Artist> CREATOR = new Creator<Artist>() {
+        @Override
+        public Artist createFromParcel(Parcel in) {
+            return new Artist(in);
+        }
+
+        @Override
+        public Artist[] newArray(int size) {
+            return new Artist[size];
+        }
+    };
 
     @Bindable
     public Long getArtistId() {
@@ -38,5 +63,21 @@ public class Artist extends BaseObservable {
     public void setName(String name) {
         this.name = name;
         notifyPropertyChanged(BR.name);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        if (artistId == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeLong(artistId);
+        }
+        parcel.writeString(name);
     }
 }
