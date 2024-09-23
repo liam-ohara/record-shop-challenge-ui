cc873c43b064df9921bc3d0a73884302df7b0099
Liam O'Hara <liam.ohara@gmail.com>
2024-09-23 15:04:08 +0100

    Feat: implement Parcelable in Publisher model class.

--- app/src/main/java/com/northcoders/record_shop_frontend/model/Publisher.java
+++ app/src/main/java/com/northcoders/record_shop_frontend/model/Publisher.java
@@ -1,12 +1,16 @@
 package com.northcoders.record_shop_frontend.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
 
 import com.google.gson.annotations.SerializedName;
 import com.northcoders.record_shop_frontend.BR;
 
-public class Publisher extends BaseObservable {
+public class Publisher extends BaseObservable implements Parcelable {
 
     @SerializedName("publisherID")
     private Long publisherId;
@@ -19,6 +23,27 @@ public class Publisher extends BaseObservable {
         this.publisherId = publisherId;
         this.name = name;
     }
+
+    protected Publisher(Parcel in) {
+        if (in.readByte() == 0) {
+            publisherId = null;
+        } else {
+            publisherId = in.readLong();
+        }
+        name = in.readString();
+    }
+
+    public static final Creator<Publisher> CREATOR = new Creator<Publisher>() {
+        @Override
+        public Publisher createFromParcel(Parcel in) {
+            return new Publisher(in);
+        }
+
+        @Override
+        public Publisher[] newArray(int size) {
+            return new Publisher[size];
+        }
+    };
 
     @Bindable
     public Long getPublisherId() {
@@ -38,5 +63,21 @@ public class Publisher extends BaseObservable {
     public void setName(String name) {
         this.name = name;
         notifyPropertyChanged(BR.name);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        if (publisherId == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeLong(publisherId);
+        }
+        parcel.writeString(name);
     }
 }
